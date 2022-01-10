4ec5768dcd7105d0797dfe00b57373354d981203
Margarita Novokh <r28611@gmail.com>
2022-01-10 23:52:00 +0100

    added Card model

---
+++ Cards/Model/Card.swift
@@ -0,0 +1,28 @@
+//
+//  Card.swift
+//  Cards
+//
+//  Created by Margarita Novokhatskaia on 10/01/2022.
+//
+
+import UIKit
+
+enum CardType: CaseIterable {
+    case circle
+    case cross
+    case square
+    case fill
+}
+
+enum CardColor: CaseIterable {
+    case red
+    case green
+    case black
+    case gray
+    case brown
+    case yellow
+    case purple
+    case orange
+}
+
+typealias Card = (type: CardType, color: CardColor)
