2bb2f0bdc66e8cf030e77112fd3ef23f7de06bbb
Margarita Novokh <r28611@gmail.com>
2022-01-10 23:30:30 +0100

    code refactoring (MVC)

---
+++ Cards/Controller/BoardGameController.swift
@@ -0,0 +1,19 @@
+//
+//  BoardGameController.swift
+//  Cards
+//
+//  Created by Margarita Novokhatskaia on 10/01/2022.
+//
+
+import UIKit
+
+class BoardGameController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
